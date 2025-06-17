6dc9e81d14be437b2b58162b13dab642dbf2c35d
Nick Baker <nbaker82@gmail.com>
2025-06-17 10:40:19 -0500

    Initial Commit

---
+++ PitchCoach AI/PitchCoach_AIApp.swift
@@ -0,0 +1,17 @@
+//
+//  PitchCoach_AIApp.swift
+//  PitchCoach AI
+//
+//  Created by Nicholas Baker on 6/17/25.
+//
+
+import SwiftUI
+
+@main
+struct PitchCoach_AIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ PitchCoach AITests/PitchCoach_AITests.swift
@@ -0,0 +1,16 @@
+//
+//  PitchCoach_AITests.swift
+//  PitchCoach AITests
+//
+//  Created by Nicholas Baker on 6/17/25.
+//
+
+import Testing
+
+struct PitchCoach_AITests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
